9a22036138f7f1bf50d42a32d0dd4423405aaba5
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2023-07-13 15:18:32 +0530

    v4

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms'
 import { faSearch, faTemperatureHigh, faTemperatureLow, faWind, faDroplet, faClock } from '@fortawesome/free-solid-svg-icons';
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, } from '@fortawesome/free-solid-svg-icons';
 import { WeatherService } from '../service/weather.service';
 import { DatePipe } from '@angular/common';
 
@@ -17,8 +17,9 @@ export class LayoutComponent implements OnInit {
   weatherIcons: any[] = [faSun, faMoon];
   weatherData: any;
 
+  submit = false;
   localTime = '';
-  time = 0;
+  time: number = 10;
   temperature = '0';
   city = '';
   country = '';
@@ -35,22 +36,27 @@ export class LayoutComponent implements OnInit {
     this.searchForm = this.formBuilder.group({
       city: [''] // Initial value of the city input field
     });
-    
+
   }
 
 
 
   onSubmit() {
-    const city = this.searchForm.value.city;
-    this.weatherService.getWeatherData(city).subscribe(
-      (data: any) => {
-        this.weatherData = data;
-        this.updateWeatherInfo();
-      },
-      (error: any) => {
-        console.error('Failed to fetch weather data:', error);
-      }
-    );
+    if (this.searchForm.value.city == '') {
+      this.submit = false;
+    } else {
+      const city = this.searchForm.value.city;
+      this.submit = true;
+      this.weatherService.getWeatherData(city).subscribe(
+        (data: any) => {
+          this.weatherData = data;
+          this.updateWeatherInfo();
+        },
+        (error: any) => {
+          console.error('Failed to fetch weather data:', error);
+        }
+      );
+    }
   }
 
   updateWeatherInfo() {
@@ -63,7 +69,8 @@ export class LayoutComponent implements OnInit {
       this.maxTemp = this.weatherData.current.feelslike_c;
       this.wind = this.weatherData.current.wind_kph;
       this.humidity = this.weatherData.current.humidity;
-      this.time = new Date(this.weatherData.localTime.localTime).getHours();
+      this.time = new Date(this.weatherData.location.localtime).getHours();
     }
+    console.log(this.time);
   }
 }
